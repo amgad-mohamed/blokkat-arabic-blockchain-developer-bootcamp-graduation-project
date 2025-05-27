e472548debe15b55ce810dea9d27d17788d695e2
Amgad <amgagmohamed38@gmail.com>
2025-05-27 15:28:49 +0300

    update project id

--- config/index.tsx
+++ config/index.tsx
@@ -3,7 +3,7 @@ import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, sepolia } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = `${process.env.NEXT_PUBLIC_PROJECT_ID}`
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
